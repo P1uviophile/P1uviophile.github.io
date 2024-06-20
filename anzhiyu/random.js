0ad3a003d6153ce1b24186c614e7f830b7a4a6e1
Joking <2083717185@qq.com>
2024-06-20 21:24:21 +0800

    Site updated: 2024-06-20 21:24:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/20/hello-world/"];function toRandomPost(){
+var posts=["2024/06/20/Hello Blog!/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
