7e115f2abec35d77943c278f12e6d3a166f5e9a3
Joking <2083717185@qq.com>
2024-06-24 21:43:56 +0800

    Site updated: 2024-06-24 21:43:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/20/Hello Blog!/","2024/06/21/自建MetingJS-API并添加cookie/","2024/06/22/Github repo + Vercel + Picgo 搭建图床/"];function toRandomPost(){
+var posts=["2024/06/20/Hello Blog!/","2024/06/21/自建MetingJS-API并添加cookie/","2024/06/22/Github repo + Vercel + Picgo 搭建图床/","2024/06/21/自建MetingJS-API并添加cookie copy/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
