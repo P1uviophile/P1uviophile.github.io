cecbceaab2584716ed830be0998a756829a862b0
Joking <2083717185@qq.com>
2024-07-17 23:14:34 +0800

    Site updated: 2024-07-17 23:14:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/28/刷题1/","2024/07/14/刷题3/","2024/06/21/自建MetingJS-API并添加cookie/","2024/07/07/刷题2/","2024/07/16/布隆过滤器和前缀树实现违禁词过滤的性能对比/"];function toRandomPost(){
+var posts=["2024/07/14/刷题3/","2024/06/28/刷题1/","2024/07/07/刷题2/","2024/07/16/布隆过滤器和前缀树实现违禁词过滤的性能对比/","2024/06/21/自建MetingJS-API并添加cookie/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
